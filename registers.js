f6875c0a764db445098de261980e782d78abcbf0
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-03-19 23:22:27 +0530

    Saving the details of users into localstorage.

---
+++ registers.js
@@ -0,0 +1,19 @@
+const registrationForm = document.getElementById("registration-form");
+registrationForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // storing the form data into local storage using object array.
+  const formData = new FormData(registrationForm);
+  const users = JSON.parse(localStorage.getItem("details")) || [];
+  const data = {};
+  for (const [key, value] of formData.entries()) {
+    data[key] = value;
+  }
+  console.log("Register button clicked");
+  users.push(data);
+  localStorage.setItem("details", JSON.stringify(users));
+});
+export function display() {
+  // console.log("hey");
+  const details = JSON.parse(localStorage.getItem("details"));
+  console.log("details:", details);
+}
--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const registrationForm = document.getElementById('registration-form');
-    const bloodRequestForm = document.getElementById('blood-request-form');
+import { display } from "./registers.js";
+const bloodRequestForm = document.getElementById("blood-request-form");
 
-    registrationForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        // Add your registration form submission logic here
-        console.log('Register button clicked');
-    });
 
-    bloodRequestForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        // Add your blood request form submission logic here
-        console.log('Request Blood button clicked');
-    });
+display();
+
+bloodRequestForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // Add your blood request form submission logic here
+  console.log("Request Blood button clicked");
 });

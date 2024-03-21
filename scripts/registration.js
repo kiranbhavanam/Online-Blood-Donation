78f5ef73acac5e35c9e5da3b7634c7763b4106dd
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-03-21 17:59:08 +0530

    created a popup when user hit submit

--- scripts/registration.js
+++ scripts/registration.js
@@ -4,4 +4,15 @@ registrationForm.addEventListener("submit", function (event) {
   event.preventDefault();
   saveUser(registrationForm);
   // window.location.href = "registeredUsers.html";
+  showPopUp();
 });
+function showPopUp() {
+  document.querySelector(".overlay").classList.add("show");
+  document.querySelector(".popup").classList.add("show");
+  const okay = document.querySelector(".ok");
+  okay.addEventListener("click", () => {
+    document.querySelector(".overlay").classList.remove("show");
+    document.querySelector(".popup").classList.remove("show");
+  });
+}
+// showPopUp();
